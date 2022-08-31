fbd5bfe4b1aeafa650e38efb5bb71f5e07928d8c
Raymundo Mosqueda <iracoon2014@gmail.com>
2022-08-31 13:58:44 -0500

    pokemon class

--- Assets/Pokemon.cs
+++ Assets/Pokemon.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
-[CreateAssetMenu(fileName ="Persona",menuName ="amerike/Persona")]
+
+[CreateAssetMenu(fileName ="Pokemon",menuName ="amerike/Pokemon")]
 
 public class Pokemon : ScriptableObject
 {
-    
+    public string p_Name;
+    public string type_1;
+    public string type_2;
+    public string abilities;
+    public int height;
+    public int weight;
+
 }
